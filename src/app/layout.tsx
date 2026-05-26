e93a145f0f1422de1eaefd9c5af6316d7858b4bb
Gabriel Maia Francisco <gabrielmaia@protonmail.com>
2026-05-26 23:12:08 +0100

    feat: integrate Vercel Speed Insights for performance monitoring in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -67,6 +68,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
